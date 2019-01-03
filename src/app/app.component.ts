af995756952be89ce92de6fdcad209006a0840ed
Kuldeep <kuldeepdayma@mobiddiction.com.au>
2019-01-03 15:35:58 +0530

    commit for 0.2.6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,12 @@ export class AppComponent {
   model: any = {};
   result: any = {};
   title = 'Testing Whiz sample !';
+  display = false;
 
   sum() {
     console.log(this.model);
+    this.display = true;
     this.result = parseInt(this.model.value1) + parseInt(this.model.value2);
-    confirm('Result is ' + this.result);
+    // confirm('Result is ' + this.result);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+// PrimeNG
+// import { ButtonModule } from 'primeng/primeng';
+import { ButtonModule } from 'primeng/components/button/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import { DialogModule } from 'primeng/components/dialog/dialog';
 
 @NgModule({
   declarations: [
@@ -15,7 +22,11 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    InputTextModule,
+    BrowserAnimationsModule,
+    DialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
